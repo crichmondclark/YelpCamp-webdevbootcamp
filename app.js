dc82f648763678170121ad2743b38788b807b8e7
Connor James <connorjamesrc@protonmail.com>
2018-08-08 05:32:23 +0100

    added mongodb for data persistance

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/yelp_camp");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.listen(3000, function () {
     console.log("yelp camp has started");
 });
 
-var campground = [{
-        name: "michaelcreek",
-        image: "images/camp1.jpeg"
-    },
-    {
-        name: "michaeltango",
-        image: "images/camp2.jpeg"
-    },
-    {
-        name: "michaeldfdsf",
-        image: "images/camp3.jpeg"
-    },
-    {
-        name: "michael",
-        image: "images/camp4.jpeg"
-    },
-    {
-        name: "ironman",
-        image: "images/camp5.jpeg"
-    }
-];
+var campgroundSchema = new mongoose.Schema( {
+    name: String,
+    image: String
+});
+
+var newCampground = mongoose.model("newCampground", campgroundSchema);
+
+
 
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds: campground});
+    newCampground.find({}, function(err, allCampgrounds) {
+        if(err) {
+            console.log("there error");
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    })
 });
 
-app.post("/campgrounds", function(req, res) {
-    var name = req.body.name;
+app.post("/campgrounds", function (req, res) {
+    var name = req.body.firstname;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campground.push(newCampground);
-    res.redirect("/campgrounds");
+    var newCampgroundy = {
+        name: name,
+        image: image
+    };
+    newCampground.create(newCampgroundy, function(err, newly) {
+        if(err) {
+            console.log("error");
+        } else {
+            res.redirect("/campgrounds");
+        }});
 });
 
-app.get("/campgrounds/new", function(req,res) {
+app.get("/campgrounds/new", function (req, res) {
     res.render("new.ejs");
 });
